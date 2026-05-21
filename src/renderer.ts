5316bfa247dc041fe627cd5311c84565fc2294bd
Blake Harms <blake@harms.haus>
2026-05-21 02:36:58 -0500

    fix: move eslint-disable to correct line for stripAnsi regex

--- src/renderer.ts
+++ src/renderer.ts
@@ -61,8 +61,8 @@ function renderStatusIcon(status: string, theme: Theme): string {
 }
 
 function stripAnsi(text: string): string {
-  // eslint-disable-next-line no-control-regex
   return text.replace(
+    /* eslint-disable-next-line no-control-regex -- ANSI escape sequences must match control characters */
     /\x1b\[[0-9;]*[a-zA-Z]|\x1b\][^\x07]*\x07|\x1b\][^\x1b]*\x1b[\x5c][^\x20-\x7E\t\r\n]/g,
     "",
   );
